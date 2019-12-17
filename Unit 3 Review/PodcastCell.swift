b1ac202d95989bb61279bb8be53be0c15279d84f
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-12-17 11:04:43 -0500

    Initial Commit

---
+++ Unit 3 Review/PodcastCell.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastCellTableViewCell.swift
+//  Unit 3 Review
+//
+//  Created by Christian Hurtado on 12/16/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class PodcastCell: UITableViewCell {
+    
+    @IBOutlet weak var podcastName: UILabel!
+    @IBOutlet weak var podcastImage: UIImageView!
+    
+   
+}
---
+++ Unit 3 Review/PodcastVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit 3 Review
+//
+//  Created by Christian Hurtado on 12/16/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class PodcastVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
