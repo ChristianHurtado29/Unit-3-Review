8413253c388b50a7a86130b425dcec1135b4a80a
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-12-17 12:09:13 -0500

    Podcast API Client complete!

---
+++ Unit 3 Review/PodcastsAPIClient.swift
@@ -0,0 +1,44 @@
+//
+//  PodcastsAPIClient.swift
+//  Unit 3 Review
+//
+//  Created by Christian Hurtado on 12/17/19.
+//  Copyright © 2019 Christian Hurtado. All rights reserved.
+//
+
+import Foundation
+
+struct PodcastsSearchAPIClient  {
+    
+    static func getPodcasts(for searchQuery: String,
+                            completion: @escaping (Result<[Podcast], AppError>) -> ()) {
+        let searchQuery = searchQuery.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ??
+        "swift"
+        
+        let podcastEndpointURL = "https://itunes.apple.com/search?media=podcast&limit=200&term=\(searchQuery)"
+        
+        guard let url = URL(string: podcastEndpointURL) else {
+            completion(.failure(.badURL(podcastEndpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let searchResults = try JSONDecoder().decode(PodcastSearch.self, from: data)
+                    
+                    let podcasts = searchResults.results
+                    
+                    completion(.success(podcasts))
+                    
+                } catch {
+                }
+            }
+        }
+    }
+}
